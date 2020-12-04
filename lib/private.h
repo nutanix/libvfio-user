02dfb99dac1ea08425552af6700579b8b1732ba7
Thanos Makatos <thanos.makatos@nutanix.com>
2020-12-04 06:57:31 -0500

    convert vfu_ctx->read to bool
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- lib/private.h
+++ lib/private.h
@@ -133,7 +133,7 @@ struct vfu_ctx {
 
     uint32_t                irq_count[VFU_DEV_NUM_IRQS];
     vfu_irqs_t              *irqs;
-    int                     ready;
+    bool                    ready;
     vfu_dev_type_t          dev_type;
 };
 
