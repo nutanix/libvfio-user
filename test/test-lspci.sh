d51c1eccde3765da44acd0e47ece79b0d39ff725
John Levon <john.levon@nutanix.com>
2021-01-13 01:15:09 +0000

    try to fix CI

--- test/test-lspci.sh
+++ test/test-lspci.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#
+# There are two different potential outputs on the distributions we test for;
+# accept either.
+#
+
 ../samples/lspci | lspci -vv -F /dev/stdin >lspci.out
-exec diff lspci.out $(dirname $0)/lspci.expected.out
+diff lspci.out $(dirname $0)/lspci.expected.out || {
+	diff lspci.out $(dirname $0)/lspci.expected.out.2
+}
+exit $?
