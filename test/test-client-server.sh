92fcfa637e5eab4af39fddd44df2741fcb758b01
Thanos Makatos <thanos.makatos@nutanix.com>
2021-01-28 10:21:38 -0500

    CI: run client/server through Valgrind
    And fail the test if Valgrind discovers any errors.
    
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- test/test-client-server.sh
+++ test/test-client-server.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+valgrind="valgrind --quiet --trace-children=yes --error-exitcode=1 --exit-on-first-error=yes --leak-check=full"
+
 sock="/tmp/vfio-user.sock"
 rm -f ${sock}*
-../samples/server -v ${sock} &
+${valgrind} ../samples/server ${sock} &
 server_pid=$!
 while [ ! -S ${sock} ]; do
 	sleep 0.1
 done
-../samples/client ${sock}
+${valgrind} ../samples/client ${sock}
 wait
