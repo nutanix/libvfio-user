43113ed462dc577f791c7d215278ab3e77c89763
Thanos Makatos <thanos.makatos@nutanix.com>
2021-01-29 09:26:53 -0500

    don't initialize region FD to 0
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- lib/libvfio-user.c
+++ lib/libvfio-user.c
@@ -1127,6 +1127,7 @@ vfu_create_ctx(vfu_trans_t trans, const char *path, int flags, void *pvt,
 {
     vfu_ctx_t *vfu_ctx = NULL;
     int err = 0;
+    size_t i;
 
     //FIXME: Validate arguments.
 
@@ -1187,6 +1188,10 @@ vfu_create_ctx(vfu_trans_t trans, const char *path, int flags, void *pvt,
             goto err_out;
         }
         vfu_ctx->fd = err;
+    }
+
+    for (i = 0; i< vfu_ctx->nr_regions; i++) {
+        vfu_ctx->reg_info[i].fd = -1;
     }
 
     return vfu_ctx;
