ca09834ca583e9571d7387e1a9677f06d6d18432
Thanos Makatos <thanos.makatos@nutanix.com>
2021-05-13 08:06:42 -0400

    use LOG_ERR instead of LOG_WARNING when failing to get dirty page bitmap
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- lib/dma.c
+++ lib/dma.c
@@ -510,13 +510,13 @@ dma_controller_dirty_page_get(dma_controller_t *dma, vfu_dma_addr_t addr,
     }
 
     if (pgsize != dma->dirty_pgsize) {
-        vfu_log(dma->vfu_ctx, LOG_WARNING, "bad page size %ld", pgsize);
+        vfu_log(dma->vfu_ctx, LOG_ERR, "bad page size %ld", pgsize);
         return ERROR_INT(EINVAL);
     }
 
     bitmap_size = get_bitmap_size(len, pgsize);
     if (bitmap_size < 0) {
-        vfu_log(dma->vfu_ctx, LOG_WARNING, "failed to get bitmap size");
+        vfu_log(dma->vfu_ctx, LOG_ERR, "failed to get bitmap size");
         return bitmap_size;
     }
 
@@ -525,7 +525,7 @@ dma_controller_dirty_page_get(dma_controller_t *dma, vfu_dma_addr_t addr,
      * expects to receive.
      */
     if (size != (size_t)bitmap_size) {
-        vfu_log(dma->vfu_ctx, LOG_WARNING, "bad bitmap size %ld != %ld", size,
+        vfu_log(dma->vfu_ctx, LOG_ERR, "bad bitmap size %ld != %ld", size,
                 bitmap_size);
         return ERROR_INT(EINVAL);
     }
