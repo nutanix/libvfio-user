1b63a5abbf7274f713e7092dbc494407b93f2c3c
Thanos Makatos <thanos.makatos@nutanix.com>
2021-05-14 05:53:10 -0400

    FIXME increase number of DMA regions
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- include/libvfio-user.h
+++ include/libvfio-user.h
@@ -58,7 +58,7 @@ extern "C" {
 #define LIB_VFIO_USER_MAJOR 0
 #define LIB_VFIO_USER_MINOR 1
 
-#define VFU_DMA_REGIONS  0x10
+#define VFU_DMA_REGIONS  0x100
 
 /* DMA addresses cannot be directly de-referenced. */
 typedef void *vfu_dma_addr_t;
