dfd2e0ecf19944a7762b2d0ef7d03e241d8fcef6
Thanos Makatos <thanos.makatos@nutanix.com>
2021-07-08 06:55:04 -0400

    basic write support for PXLC, PXSC, PXRS, and PXSC2
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- lib/pci_caps.c
+++ lib/pci_caps.c
@@ -293,6 +293,44 @@ handle_px_pxdc_write(vfu_ctx_t *vfu_ctx, struct pxcap *px,
 }
 
 static int
+handle_px_pxlc_write(vfu_ctx_t *vfu_ctx UNUSED, struct pxcap *px,
+                     const union pxlc *const p)
+{
+    assert(px != NULL);
+    assert(p != NULL);
+
+    /* FIXME RCB only writeable for endpoints and bridges */
+    /* FIXME must preserve reserved bits */
+    px->pxlc.raw = p->raw;
+    return 0;
+}
+
+static int
+handle_px_pxsc_write(vfu_ctx_t *vfu_ctx UNUSED, struct pxcap *px,
+                     const struct pxsc *const p)
+{
+    assert(px != NULL);
+    assert(p != NULL);
+
+    /* FIXME */
+    px->pxsc.stuff = p->stuff;
+    return 0;
+}
+
+static int
+handle_px_pxrc_write(vfu_ctx_t *vfu_ctx UNUSED, struct pxcap *px,
+                     const struct pxrc *const p)
+{
+    assert(px != NULL);
+    assert(p != NULL);
+
+    /* FIXME  */
+    /* FIXME must preserve reserved bits */
+    px->pxrc.stuff = p->stuff;
+    return 0;
+}
+
+static int
 handle_px_pxdc2_write(vfu_ctx_t *vfu_ctx, struct pxcap *px,
                       const union pxdc2 *const p)
 {
@@ -327,10 +365,18 @@ handle_px_write_2_bytes(vfu_ctx_t *vfu_ctx, struct pxcap *px, char *buf,
     switch (off) {
     case offsetof(struct pxcap, pxdc):
         return handle_px_pxdc_write(vfu_ctx, px, (union pxdc *)buf);
+    case offsetof(struct pxcap, pxlc):
+        return handle_px_pxlc_write(vfu_ctx, px, (union pxlc *)buf);
+    case offsetof(struct pxcap, pxsc):
+        return handle_px_pxsc_write(vfu_ctx, px, (struct pxsc *)buf);
+    case offsetof(struct pxcap, pxrc):
+        return handle_px_pxrc_write(vfu_ctx, px, (struct pxrc *)buf);
     case offsetof(struct pxcap, pxdc2):
         return handle_px_pxdc2_write(vfu_ctx, px, (union pxdc2 *)buf);
     case offsetof(struct pxcap, pxlc2):
         return handle_px_pxlc2_write(vfu_ctx, px, (struct pxlc2 *)buf);
+    case offsetof(struct pxcap, pxsc2): /* RsvdZ */
+        return 0;
     }
     return ERROR_INT(EINVAL);
 }
