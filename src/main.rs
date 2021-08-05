1bc7cee3e726311ebbdce67101bd024f5cc32331
Thanos Makatos <thanos.makatos@nutanix.com>
2021-08-05 16:19:06 -0400

    hack to enable logging
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate libc;
 
-use libc::c_char;
+use libc::{c_int, c_char};
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::ffi::c_void;
@@ -14,6 +14,7 @@ use std::ptr;
 use libvfio_user_sys::vfu_ctx_t;
 use libvfio_user_sys::vfu_create_ctx;
 use libvfio_user_sys::vfu_setup_log;
+use libvfio_user_sys::vfu_log_fn_t;
 use libvfio_user_sys::vfu_pci_init;
 use libvfio_user_sys::vfu_setup_region;
 use libvfio_user_sys::vfu_pci_set_id;
@@ -26,26 +27,43 @@ use libvfio_user_sys::vfu_dev_irq_type_VFU_DEV_INTX_IRQ;
 use libvfio_user_sys::vfu_realize_ctx;
 use libvfio_user_sys::vfu_attach_ctx;
 use libvfio_user_sys::vfu_run_ctx;
+use libvfio_user_sys::iovec;
+
+unsafe extern "C" fn _log(
+    vfu_ctx: *mut vfu_ctx_t,
+    level: ::std::os::raw::c_int,
+    msg: *const ::std::os::raw::c_char)
+{
+    println!("{}", CStr::from_ptr(msg).to_string_lossy().into_owned());
+}
+
 
 fn main() {
     let c_str = CString::new("/var/run/vfio-user.sock").unwrap();
     let sock: *const c_char = c_str.as_ptr() as *const c_char;
     let p: *mut c_void = ptr::null_mut();
     unsafe {
-        // FIXME check return values
         let vfu_ctx = vfu_create_ctx(vfu_trans_t_VFU_TRANS_SOCK, sock, 0, p,
                                      vfu_dev_type_t_VFU_DEV_TYPE_PCI);
-	let mut ret = vfu_pci_init(vfu_ctx,
+	// FIXME check vfu_ctx non-null
+	let mut ret;
+        ret = libvfio_user_sys::vfu_setup_log(vfu_ctx, _log, 7); // FIXME LOG_DEBUG
+	ret = vfu_pci_init(vfu_ctx,
             libvfio_user_sys::vfu_pci_type_t_VFU_PCI_TYPE_CONVENTIONAL,
             0, // FIXME PCI_HEADER_TYPE_NORMAL
             0);
+	assert!(ret == 0);
         libvfio_user_sys::vfu_pci_set_id(vfu_ctx, 0x494f, 0x0dc8, 0x0, 0x0);
         // FIXME setup region
         ret = libvfio_user_sys::vfu_setup_device_nr_irqs(vfu_ctx,
             vfu_dev_irq_type_VFU_DEV_INTX_IRQ, 1);
+	assert!(ret == 0);
         ret = libvfio_user_sys::vfu_realize_ctx(vfu_ctx);
+	assert!(ret == 0);
         ret = libvfio_user_sys::vfu_attach_ctx(vfu_ctx);
+	assert!(ret == 0);
 	ret = libvfio_user_sys::vfu_run_ctx(vfu_ctx);
+	assert!(ret == 0);
     }
 }
 
