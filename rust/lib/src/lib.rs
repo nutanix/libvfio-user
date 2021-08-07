3d1173662335a01a993413fef5b6967d9c8c1cd9
John Levon <john.levon@nutanix.com>
2021-08-07 11:55:12 +0000

    quieten warnings from bindgen code

--- rust/lib/src/lib.rs
+++ rust/lib/src/lib.rs
@@ -1,4 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+/*
+ * FIXME: turn off E0133 for now; se 
+ * https://github.com/rust-lang/rust-bindgen/issues/1491
+ */
+#![allow(warnings)]
 include!("./bindings.rs");
