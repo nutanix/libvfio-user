0fca1b67ed7898b5c45d8cb496e5d9033890fce2
John Levon <john.levon@nutanix.com>
2021-08-07 00:30:38 +0000

    cargo fmt

--- rust/gpio-pci-idio-16/src/main.rs
+++ rust/gpio-pci-idio-16/src/main.rs
@@ -1,9 +1,9 @@
 extern crate libc;
 
-use libc::{c_int, c_char};
+use libc::{c_char, c_int, c_longlong};
+use std::ffi::c_void;
 use std::ffi::CStr;
 use std::ffi::CString;
-use std::ffi::c_void;
 use std::str;
 
 extern crate libvfio_user_sys;
@@ -11,29 +11,29 @@ extern crate libvfio_user_sys;
 use std::env;
 use std::ptr;
 
-use libvfio_user_sys::vfu_ctx_t;
+use libvfio_user_sys::iovec;
+use libvfio_user_sys::vfu_attach_ctx;
 use libvfio_user_sys::vfu_create_ctx;
-use libvfio_user_sys::vfu_setup_log;
+use libvfio_user_sys::vfu_ctx_t;
+use libvfio_user_sys::vfu_dev_irq_type_VFU_DEV_INTX_IRQ;
+use libvfio_user_sys::vfu_dev_type_t_VFU_DEV_TYPE_PCI;
 use libvfio_user_sys::vfu_log_fn_t;
 use libvfio_user_sys::vfu_pci_init;
-use libvfio_user_sys::vfu_setup_region;
 use libvfio_user_sys::vfu_pci_set_id;
-use libvfio_user_sys::vfu_trans_t_VFU_TRANS_SOCK;
-use libvfio_user_sys::vfu_dev_type_t_VFU_DEV_TYPE_PCI;
 use libvfio_user_sys::vfu_pci_type_t_VFU_PCI_TYPE_CONVENTIONAL;
+use libvfio_user_sys::vfu_realize_ctx;
+use libvfio_user_sys::vfu_run_ctx;
+use libvfio_user_sys::vfu_setup_log;
+use libvfio_user_sys::vfu_setup_region;
+use libvfio_user_sys::vfu_trans_t_VFU_TRANS_SOCK;
 use libvfio_user_sys::VFU_PCI_DEV_BAR2_REGION_IDX;
 use libvfio_user_sys::VFU_REGION_FLAG_RW;
-use libvfio_user_sys::vfu_dev_irq_type_VFU_DEV_INTX_IRQ;
-use libvfio_user_sys::vfu_realize_ctx;
-use libvfio_user_sys::vfu_attach_ctx;
-use libvfio_user_sys::vfu_run_ctx;
-use libvfio_user_sys::iovec;
 
 unsafe extern "C" fn _log(
     vfu_ctx: *mut vfu_ctx_t,
-    level: ::std::os::raw::c_int,
-    msg: *const ::std::os::raw::c_char)
-{
+    level: c_int,
+    msg: *const ::std::os::raw::c_char,
+) {
     println!("{}", CStr::from_ptr(msg).to_string_lossy().into_owned());
 }
 
@@ -41,9 +41,9 @@ unsafe extern "C" fn bar2(
     vfu_ctx: *mut vfu_ctx_t,
     buf: *mut c_char,
     count: usize,
-    offset: ::std::os::raw::c_longlong,
-    is_write: bool) -> isize
-{
+    offset: c_longlong,
+    is_write: bool,
+) -> isize {
     static mut pin: i8 = 0;
 
     if offset == 0 && !is_write {
@@ -61,34 +61,41 @@ fn main() {
     let p: *mut c_void = ptr::null_mut();
     let null_iovec: *mut libvfio_user_sys::iovec = std::ptr::null_mut();
     unsafe {
-        let vfu_ctx = vfu_create_ctx(vfu_trans_t_VFU_TRANS_SOCK, sock, 0, p,
-                                     vfu_dev_type_t_VFU_DEV_TYPE_PCI);
-	// FIXME check vfu_ctx non-null
-	let mut ret;
+        let vfu_ctx = vfu_create_ctx(
+            vfu_trans_t_VFU_TRANS_SOCK,
+            sock,
+            0,
+            p,
+            vfu_dev_type_t_VFU_DEV_TYPE_PCI,
+        );
+        // FIXME check vfu_ctx non-null
+        let mut ret;
         ret = libvfio_user_sys::vfu_setup_log(vfu_ctx, _log, 7); // FIXME LOG_DEBUG
-	ret = vfu_pci_init(vfu_ctx,
+        ret = vfu_pci_init(
+            vfu_ctx,
             libvfio_user_sys::vfu_pci_type_t_VFU_PCI_TYPE_CONVENTIONAL,
             0, // FIXME PCI_HEADER_TYPE_NORMAL
-            0);
-	assert!(ret == 0);
+            0,
+        );
+        assert!(ret == 0);
         libvfio_user_sys::vfu_pci_set_id(vfu_ctx, 0x494f, 0x0dc8, 0x0, 0x0);
-        ret = vfu_setup_region(vfu_ctx,
-	    2, // VFU_PCI_DEV_BAR2_REGION_IDX,
-	    0x100,
-            bar2,
-	    3, // VFU_REGION_FLAG_RW
-	    null_iovec,
-	    0, -1, 0);
-	assert!(ret == 0);
-        ret = libvfio_user_sys::vfu_setup_device_nr_irqs(vfu_ctx,
-            vfu_dev_irq_type_VFU_DEV_INTX_IRQ, 1);
-	assert!(ret == 0);
+        ret = vfu_setup_region(
+            vfu_ctx, 2, // VFU_PCI_DEV_BAR2_REGION_IDX,
+            0x100, bar2, 3, // VFU_REGION_FLAG_RW
+            null_iovec, 0, -1, 0,
+        );
+        assert!(ret == 0);
+        ret = libvfio_user_sys::vfu_setup_device_nr_irqs(
+            vfu_ctx,
+            vfu_dev_irq_type_VFU_DEV_INTX_IRQ,
+            1,
+        );
+        assert!(ret == 0);
         ret = libvfio_user_sys::vfu_realize_ctx(vfu_ctx);
-	assert!(ret == 0);
+        assert!(ret == 0);
         ret = libvfio_user_sys::vfu_attach_ctx(vfu_ctx);
-	assert!(ret == 0);
-	ret = libvfio_user_sys::vfu_run_ctx(vfu_ctx);
-	assert!(ret == 0);
+        assert!(ret == 0);
+        ret = libvfio_user_sys::vfu_run_ctx(vfu_ctx);
+        assert!(ret == 0);
     }
 }
-
