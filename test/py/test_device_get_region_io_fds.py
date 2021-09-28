e7f0fc73cdab811948adf5227c04f722a7c3105b
John Levon <john.levon@nutanix.com>
2021-09-28 17:34:12 +0100

    disable VFIO_USER_DEVICE_GET_REGION_IO_FDS on invalid region (#607)
    If a region is not set up, asking for its iofds should fail with EINVAL.
    
    Co-authored-by: John Levon <john.levon@nutanix.com>
    Signed-off-by: John Levon <john.levon@nutanix.com>
    Reviewed-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- test/py/test_device_get_region_io_fds.py
+++ test/py/test_device_get_region_io_fds.py
@@ -140,7 +140,7 @@ def test_device_get_region_io_fds_no_regions_setup():
                                 index = VFU_PCI_DEV_BAR3_REGION_IDX, count = 0)
 
     ret = msg(ctx, sock, VFIO_USER_DEVICE_GET_REGION_IO_FDS, payload,
-              expect = 0)
+              expect = errno.EINVAL)
 
 def test_device_get_region_io_fds_region_no_mmap():
 
