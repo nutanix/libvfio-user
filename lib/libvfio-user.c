10a9533be157ed62b7d1584be2eee541cacfa1c3
Thanos Makatos <thanos.makatos@nutanix.com>
2022-02-24 11:38:57 +0000

    additional logging around dirty pages
    Signed-off-by: Thanos Makatos <thanos.makatos@nutanix.com>

--- lib/libvfio-user.c
+++ lib/libvfio-user.c
@@ -942,7 +942,12 @@ handle_dirty_pages_get(vfu_ctx_t *vfu_ctx, vfu_msg_t *msg)
             msg->out_size = 0;
             return ERROR_INT(ret);
         }
+    } else {
+        vfu_log(vfu_ctx, LOG_ERR,
+                "dirty pages: get %#lx-%#lx: buffer too small (%u < %lu)",
+                range_in->iova, range_in->size, dirty_pages_in->argsz, argsz);
     }
+
     return 0;
 }
 
