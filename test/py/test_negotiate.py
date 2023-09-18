c1c06c60efb264d7e55ac1b5348dda9e5dc79094
John Levon <john.levon@nutanix.com>
2023-09-18 13:39:03 +0000

    fix up for migration checks

--- test/py/test_negotiate.py
+++ test/py/test_negotiate.py
@@ -55,10 +55,6 @@ def test_server_setup():
     ctx = vfu_create_ctx()
     assert ctx is not None
 
-    ret = vfu_setup_region(ctx, index=VFU_PCI_DEV_MIGR_REGION_IDX,
-                           size=2 << PAGE_SHIFT, flags=VFU_REGION_FLAG_RW)
-    assert ret == 0
-
     ret = vfu_setup_device_migration_callbacks(ctx)
     assert ret == 0
 
