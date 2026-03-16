nutanix/libvfio-user

commit 23da2d88ece6e85042d541a8bd8b704742e2fc32

Make libvfio-user headers self-contained (#834)
Allows running tools like clang-include-cleaner without error.

Signed-off-by: Peter Foley <pefoley@google.com>

include/pci_caps/msix.h
@@ -33,7 +33,11 @@
 #ifndef LIB_VFIO_USER_PCI_CAPS_MSIX_H
 #define LIB_VFIO_USER_PCI_CAPS_MSIX_H
 
+#include <stdint.h>
+
 #include <linux/pci_regs.h>
+
+#include "pci_caps/common.h"
 
 #ifdef __cplusplus
 extern "C" {

include/pci_caps/common.h
@@ -33,6 +33,7 @@
 
 #include <linux/pci_regs.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {

lib/common.h
@@ -39,6 +39,7 @@
 
 #include <errno.h>
 #include <limits.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/uio.h>

lib/migration_priv.h
@@ -31,7 +31,13 @@
 #ifndef LIB_VFIO_USER_MIGRATION_PRIV_H
 #define LIB_VFIO_USER_MIGRATION_PRIV_H
 
+#include <stddef.h>
+
 #include <linux/vfio.h>
+
+#include "common.h"
+#include "libvfio-user.h"
+#include "vfio-user.h"
 
 struct migration {
     enum vfio_user_device_mig_state state;

lib/pci.h
@@ -33,6 +33,8 @@
 #ifndef LIB_VFIO_USER_PCI_H
 #define LIB_VFIO_USER_PCI_H
 
+#include <assert.h>
+
 #include "libvfio-user.h"
 #include "private.h"
 

commit 518dda621124796e104887a8ea7dcb1cd666ac6d

Correct relative include
This header file should pull in common.h from the local dir, instead of
relying upon include search order with include/pci_caps. This fixes the
build of SPDK.

Fixes: 23da2d88 ("Make libvfio-user headers self-contained (#834)")
Signed-off-by: John Levon <john.levon@nutanix.com>

include/pci_caps/msix.h
@@ -37,7 +37,7 @@
 
 #include <linux/pci_regs.h>
 
-#include "pci_caps/common.h"
+#include "common.h"
 
 #ifdef __cplusplus
 extern "C" {